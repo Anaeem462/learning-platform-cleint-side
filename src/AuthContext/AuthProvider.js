ce292390a695eea6dd2cb0176c2e0eac3dbaff1c
sifa43 <sifatunnoor@gmail.com>
2022-10-26 01:20:59 +0600

    auth context added

--- src/AuthContext/AuthProvider.js
+++ src/AuthContext/AuthProvider.js
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createContext } from "react";
+
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../Firebase/auto-firebase";
+
 const AuthContext = createContext();
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
 const AuthProvider = ({ children }) => {
-  const user = { userName: "naeem" };
-  const authinfo = { user };
+  const [user, setUser] = useState();
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const googleSignIn = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+  const githubSignIn = () => {
+    return signInWithPopup(auth, githubProvider);
+  };
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        setUser(currentUser);
+        // ...
+      } else {
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const authinfo = {
+    createUser,
+    signIn,
+    user,
+    googleSignIn,
+    githubSignIn,
+    logOut,
+  };
   return (
     <AuthContext.Provider value={authinfo}>{children}</AuthContext.Provider>
   );
