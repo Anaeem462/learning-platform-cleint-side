5339a215ebe00f11425f46ba520c9039c0490879
sifa43 <sifatunnoor@gmail.com>
2022-10-27 01:20:52 +0600

    without home component complete

--- src/Firebase/auto-firebase.js
+++ src/Firebase/auto-firebase.js
@@ -17,3 +17,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export default app;
+
+//  apiKey: "AIzaSyDeKKtKU4vp41ozSxUmhonbveLvWQYL6mc",
+//   authDomain: "course-authentication-44aa0.firebaseapp.com",
+//   projectId: "course-authentication-44aa0",
+//   storageBucket: "course-authentication-44aa0.appspot.com",
+//   messagingSenderId: "612946639211",
+//   appId: "1:612946639211:web:4b52d373cb6ac81ce5f42e",
